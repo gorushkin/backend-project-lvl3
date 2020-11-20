46684df366b30ce2ae57aa2ef7b86fd83ea90bdf
gorushkin <gorushkin@gmail.com>
2020-11-20 16:12:40 +0300

    убрал склейку адреса

--- src/index.js
+++ src/index.js
@@ -12,49 +12,50 @@ const getFileName = (url) => {
 const getPath = (folder, fileName) => path.join(folder, fileName);
 
 export default (output, url) => {
+  const targetUrl = new URL(url);
   const pathToProject = output || process.cwd();
   const projectName = getFileName(url);
   const fileName = `${projectName}.html`;
-  const folderName = `${projectName}_files`;
+  const assetsFolder = `${projectName}_files`;
   const filePath = getPath(pathToProject, fileName);
-  const folderPath = getPath(pathToProject, folderName);
+  const folderPath = getPath(pathToProject, assetsFolder);
 
-  const createFolder = () => {
-    const outPath = output ? folderPath : folderName;
-    return fs.promises.mkdir(outPath, { recursive: true });
-  };
+  const createAssetsFolder = () => fs
+    .promises.mkdir(getPath(output, assetsFolder), { recursive: true }).catch(() => {
+      throw new Error('There is no such directory');
+    });
 
   const getHtmlFile = () => axios
-    .get(url)
+    .get(targetUrl.href)
     .then((response) => cheerio.load(response.data.toString(), { decodeEntities: false }));
 
   const getImgSources = (html) => {
     const imgSources = [];
     html('img').each((i, elem) => {
-      const src = url + html(elem).attr('src');
-      const name = path.posix.basename(src);
-      html(elem).attr('src', getPath(folderName, name));
-      imgSources.push({ name, src });
+      const source = new URL(`${targetUrl.pathname + html(elem).attr('src')}`, targetUrl.href).href;
+      const name = path.posix.basename(source);
+      html(elem).attr('src', getPath(assetsFolder, name));
+      imgSources.push({ name, source });
     });
-    return fs.promises.writeFile(filePath, html.html(), 'utf-8')
-      .then(() => Promise.resolve(imgSources));
+    return fs.promises
+      .writeFile(filePath, html.html(), 'utf-8')
+      .then(() => imgSources);
   };
 
   const downloadImages = (list) => {
-    const promises = list
-      .map(({ name, src }) => axios
-        .get(src, {
-          responseType: 'arraybuffer',
-        })
-        .then((response) => {
-          const imgPath = getPath(folderPath, name);
-          fs.promises.writeFile(imgPath, response.data, 'utf-8');
-        }));
+    const promises = list.map(({ name, source }) => axios
+      .get(source, {
+        responseType: 'arraybuffer',
+      })
+      .then((response) => {
+        const imgPath = getPath(folderPath, name);
+        fs.promises.writeFile(imgPath, response.data, 'utf-8');
+      }));
     const promise = Promise.all(promises);
     return promise;
   };
 
-  return createFolder()
+  return createAssetsFolder()
     .then(getHtmlFile)
     .then(getImgSources)
     .then(downloadImages)
