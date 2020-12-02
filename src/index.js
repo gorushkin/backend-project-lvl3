0f2d2915b665ba03f1cf97c1f35deaf690dc66b7
gorushkin <gorushkin@gmail.com>
2020-12-02 11:04:02 +0300

    переделал ф-цию сбора имени

--- src/index.js
+++ src/index.js
@@ -9,11 +9,9 @@ const elements = {
   script: 'src',
 };
 
-const getSlugName = (str) => str.replace(/[^A-Za-z0-9]/g, '-').replace(/-$/i, '');
-
-const getParsedUrl = (url) => {
+const convertUrlToSlugName = (url) => {
   const { host, pathname } = new URL(url);
-  return `${host}${pathname}`;
+  return (`${host}${pathname}`).replace(/[^A-Za-z0-9]/g, '-').replace(/-$/i, '');
 };
 
 const createAssetsFolder = (assetsFolderPath) => fs
@@ -28,45 +26,45 @@ const getHtmlFile = (targetUrl) => axios
 const getElementFilename = (source) => {
   const { ext, name, dir } = path.parse(source);
   const extension = ext || '.html';
-  const filename = getSlugName(getParsedUrl(path.join(dir, name)));
+  const filename = convertUrlToSlugName(path.join(dir, name));
   return `${filename}${extension}`;
 };
 
-const isSourceLocal = (source, url) => (new URL(source, url)).origin === url;
-
-const getSources = (parsedDom, url, assetsFolderName, assetsFolderPath) => {
+const getSources = (parsedDom, url, assetsFolderName) => {
   const sources = Object.entries(elements).reduce((acc, [itemName, itemSrcAttribute]) => {
     const itemSources = parsedDom(itemName)
       .toArray()
       .map((item) => {
-        const source = (new URL(item.attribs[itemSrcAttribute], url)).href;
+        const source = (new URL(item.attribs[itemSrcAttribute], url));
         return { ...item, source };
       })
-      .filter(({ source }) => isSourceLocal(source, url.origin))
+      .filter(({ source }) => source.origin === url.origin)
       .map((item) => {
-        const filename = getElementFilename(item.source);
-        const itemPath = path.join(assetsFolderPath, filename);
+        const filename = getElementFilename(item.source.href);
         parsedDom(item).attr(itemSrcAttribute, path.join(assetsFolderName, filename));
-        return { source: item.source, itemPath };
+        return { source: item.source.href, filename };
       });
     return [...acc, ...itemSources];
   }, []);
   return [parsedDom, sources];
 };
 
-const downloadElements = (parsedDom, sources, filePath) => {
+const downloadElements = (parsedDom, sources, filePath, assetsFolderPath) => {
   const promises = sources.map((item) => axios
     .get(item.source, {
       responseType: 'arraybuffer',
     })
-    .then((response) => fs.promises.writeFile(item.itemPath, response.data, 'utf-8').catch(console.error)));
+    .then((response) => {
+      const itemPath = path.join(assetsFolderPath, item.filename);
+      return fs.promises.writeFile(itemPath, response.data, 'utf-8').catch(console.error);
+    }));
   return fs.promises.writeFile(filePath, parsedDom.html(), 'utf-8').then(() => Promise.all(promises));
 };
 
 export default (output, url) => {
   const targetUrl = new URL(url);
   const pathToProject = path.resolve(process.cwd(), output);
-  const projectName = getSlugName(getParsedUrl(url));
+  const projectName = convertUrlToSlugName(url);
   const filePath = path.join(pathToProject, `${projectName}.html`);
   const assetsFolderName = `${projectName}_files`;
   const assetsFolderPath = path.join(pathToProject, assetsFolderName);
@@ -77,12 +75,12 @@ export default (output, url) => {
       parsedDom,
       targetUrl,
       assetsFolderName,
-      assetsFolderPath,
     ))
     .then(([parsedDom, sources]) => (downloadElements(
       parsedDom,
       sources,
       filePath,
+      assetsFolderPath,
     )))
     .then(() => pathToProject)
     .catch(console.error);
