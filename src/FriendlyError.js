29eb820cce76dfa24372dd1c6314a9a5c2bed0a9
gorushkin <gorushkin@gmail.com>
2020-12-31 13:57:18 +0300

    работа над ошибками2

--- src/FriendlyError.js
+++ src/FriendlyError.js
@@ -8,16 +8,33 @@ const translateErrorCode = (code) => systemErrorMapping[code]?.description || de
 const messageBuilders = [
   {
     check: (error) => error?.isAxiosError,
+
     buildMessage: ({
       code, response, config, message,
-    }) => _.compact([translateErrorCode(code), response, config, message]).join('\n'),
+    }) => _.compact([
+      'Error occurred:',
+      `Message: ${message}`,
+      code && `Description: ${translateErrorCode(code)}`,
+      code && `Code: ${code}`,
+      `URL: ${config.url}`,
+      `StatusCode: ${response.status}`,
+      response.statusText && `StatusText: ${response.statusText}`,
+    ]).join('\n'),
   },
   {
     check: (error) => !!error?.code,
 
     buildMessage: ({
-      code, response, config, message,
-    }) => _.compact([translateErrorCode(code), response, config, message]).join('\n'),
+      code, address, path, port, message,
+    }) => _.compact([
+      'Error occurred:',
+      `Message: ${message}`,
+      `Description: ${translateErrorCode(code)}`,
+      `Code: ${code}`,
+      address && `URL: ${address}`,
+      port && `Port: ${port}`,
+      path && `Path: ${path}`,
+    ]).join('\n'),
   },
   {
     check: () => true,
